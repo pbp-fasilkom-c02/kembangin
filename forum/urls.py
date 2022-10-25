3c84eeb33f0164cb931923f761d64a41aece0f4e
marcellinuselbert <elbertmarcellinus@gmail.com>
2022-10-25 23:48:12 +0700

    Forum index done, still need to work on forum detail

--- forum/urls.py
+++ forum/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from forum.views import get_forums_json, show_forum_index
+from forum.views import add_forum, delete_forum, get_forums_json, show_forum_index
 
 
 app_name = 'forum'
 
 urlpatterns = [
     path('',show_forum_index, name="show_forum_index"),
-    path('json',get_forums_json, name="get_forums_json")
-    # path('', show_profile, name='show_profile'),
+    path('json',get_forums_json, name="get_forums_json"),
+    path('add', add_forum, name='add_forum'),
+    path('delete/<id>',delete_forum,name='delete_forum')
     # path('/<id>',forum, name="forum")
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,9 +1,12 @@
+
 from django.http import JsonResponse
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseBadRequest
+from django.shortcuts import render,get_object_or_404
 from forum.models import Forum, ForumReply
 from forum.forms import ForumForm
-
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+import datetime
 
 
 
@@ -31,6 +34,7 @@ def get_forums_json(request):
             'author': forum.author.username,
             'description': forum.description,
             'created_at' : forum.created_at,
+            'pk': forum.pk,
             'replies': replies,
             
         })
@@ -39,9 +43,38 @@ def get_forums_json(request):
     return JsonResponse(list_of_forums,safe=False)
 
     
+@login_required(login_url='/login')
+@csrf_exempt
+def add_forum(request):
+    print("dsaasdas")
+    if request.method == "POST":
+        form = ForumForm(request.POST)
+        if form.is_valid():
+            question = form.cleaned_data["question"]
+            description = form.cleaned_data["description"]
+            forum = Forum.objects.create(question=question,description=description,created_at=datetime.datetime.now(),author=request.user)
 
+            data = {
+            "pk": forum.pk,
+            "question": forum.question,
+            "description": forum.description,
+            "author": forum.author.username,
+            "created_at": forum.created_at
+            }
+            
 
+        return JsonResponse(data)
 
+    return HttpResponseBadRequest()
+
+@login_required(login_url='/login')
+@csrf_exempt
+def delete_forum(request,id):
+    if request.method == "DELETE":
+        task = get_object_or_404(Forum, id = id)
+        task.delete()
+
+    return HttpResponse(status=202)
 
 def show_forum_detail(request):
 
