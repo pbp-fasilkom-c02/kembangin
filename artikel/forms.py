b230f2dabfb6c8ce1e12043651bde68331a84ec4
risalstr <risa.lestari2002@gmail.com>
2022-11-01 04:58:07 +0700

    fix: delete unnecessary code

--- artikel/forms.py
+++ artikel/forms.py
@@ -1,4 +1,2 @@
 from django import forms
-
-class ReplyThread(forms.Form):
-    comment = forms.CharField(label='comment', max_length=150,widget=forms.Textarea(attrs={"rows":"5"}))
+from artikel.models import Artikel
