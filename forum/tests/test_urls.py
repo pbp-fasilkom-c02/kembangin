b6553a3c3f515c031109a5e94b29d79978b3a864
marcellinuselbert <elbertmarcellinus@gmail.com>
2022-11-02 22:05:07 +0700

    implement coverage

--- forum/tests/test_urls.py
+++ forum/tests/test_urls.py
@@ -6,19 +6,23 @@ from forum.models import Forum
 import datetime
 
 class TestUrls(TestCase):
-    user = User.objects.create_user(username="dummyjj", email="dummyjj@dummy.com", password="dummys12345")
-    forum = Forum.objects.create(question="This is test question",description="Lorem ipsum",created_at=datetime.datetime.now(),author=user)
+    
     def test_forum_url_is_resolved(self):
+        
         url = reverse("forum:show_forum_index")
         self.assertEquals(resolve(url).func,show_forum_index)
     
     def test_forum_detail_url_is_resolved(self):
         user = User.objects.create_user(username="dummyaa", email="dummyaa@dummy.com", password="dummy12345") 
-        url = reverse("forum:show_forum_detail",kwargs={"pk":self.forum.pk})
+        user = User.objects.create_user(username="dummyjj", email="dummyjj@dummy.com", password="dummys12345")
+        forum = Forum.objects.create(question="This is test question",description="Lorem ipsum",created_at=datetime.datetime.now(),author=user)
+        url = reverse("forum:show_forum_detail",kwargs={"pk":forum.pk})
         self.assertEquals(resolve(url).func,show_forum_detail)
 
     def test_get_forum_by_pk_url_is_resolved(self):
-        url = reverse("forum:get_forum_by_pk",kwargs={"pk":self.forum.pk})
+        user = User.objects.create_user(username="dummyjj", email="dummyjj@dummy.com", password="dummys12345")
+        forum = Forum.objects.create(question="This is test question",description="Lorem ipsum",created_at=datetime.datetime.now(),author=user)
+        url = reverse("forum:get_forum_by_pk",kwargs={"pk":forum.pk})
         self.assertEquals(resolve(url).func,get_forum_by_pk)
     
 
@@ -31,22 +35,32 @@ class TestUrls(TestCase):
         self.assertEquals(resolve(url).func,add_forum)
     
     def test_delete_forum_url_is_resolved(self):
-        url = reverse("forum:delete_forum",kwargs={"id":self.forum.pk})
+        user = User.objects.create_user(username="dummyjj", email="dummyjj@dummy.com", password="dummys12345")
+        forum = Forum.objects.create(question="This is test question",description="Lorem ipsum",created_at=datetime.datetime.now(),author=user)
+        url = reverse("forum:delete_forum",kwargs={"id":forum.pk})
         self.assertEquals(resolve(url).func,delete_forum)
 
 
     def test_add_comment_url_is_resolved(self):
-        url = reverse("forum:add_comment",kwargs={"pk":self.forum.pk})
+        user = User.objects.create_user(username="dummyjj", email="dummyjj@dummy.com", password="dummys12345")
+        forum = Forum.objects.create(question="This is test question",description="Lorem ipsum",created_at=datetime.datetime.now(),author=user)
+        url = reverse("forum:add_comment",kwargs={"pk":forum.pk})
         self.assertEquals(resolve(url).func,add_comment)
 
     def test_delete_comment_url_is_resolved(self):
-        url = reverse("forum:delete_comment", kwargs={"pk":self.forum.pk})
+        user = User.objects.create_user(username="dummyjj", email="dummyjj@dummy.com", password="dummys12345")
+        forum = Forum.objects.create(question="This is test question",description="Lorem ipsum",created_at=datetime.datetime.now(),author=user)
+        url = reverse("forum:delete_comment", kwargs={"pk":forum.pk})
         self.assertEquals(resolve(url).func,delete_comment)
 
     def test_upvote_url_is_resolved(self):
-        url = reverse("forum:handle_vote", kwargs={"pk":self.forum.pk, "action":"up"})
+        user = User.objects.create_user(username="dummyjj", email="dummyjj@dummy.com", password="dummys12345")
+        forum = Forum.objects.create(question="This is test question",description="Lorem ipsum",created_at=datetime.datetime.now(),author=user)
+        url = reverse("forum:handle_vote", kwargs={"pk":forum.pk, "action":"up"})
         self.assertEquals(resolve(url).func,handle_vote)
 
     def test_downvote_url_is_resolved(self):
-        url = reverse("forum:handle_vote", kwargs={"pk":self.forum.pk, "action":"down"})
+        user = User.objects.create_user(username="dummyjj", email="dummyjj@dummy.com", password="dummys12345")
+        forum = Forum.objects.create(question="This is test question",description="Lorem ipsum",created_at=datetime.datetime.now(),author=user)
+        url = reverse("forum:handle_vote", kwargs={"pk":forum.pk, "action":"down"})
         self.assertEquals(resolve(url).func,handle_vote)
