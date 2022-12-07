e434f3833488575ba4a32b45d6391a0aca3146d9
marcellinuselbert <elbertmarcellinus@gmail.com>
2022-12-07 14:03:38 +0700

    Restore deleted view on forum

--- forum/urls.py
+++ forum/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('delete/<int:id>',delete_forum,name='delete_forum'),
     path('delete-forum/<int:id>/<str:current_username>',delete_forum_flutter,name='delete_forum_flutter'),
     path('<int:pk>',show_forum_detail, name="show_forum_detail"),
+    path('<int:pk>/add-comment',add_comment, name="add_comment"),
     path('<int:pk>/add-comment-flutter',add_comment_flutter, name="add_comment_flutter"),
     path('<int:pk>/delete-comment/',delete_comment, name="delete_comment"),
     path('<int:pk>/vote/<str:action>',handle_vote,name="handle_vote")
