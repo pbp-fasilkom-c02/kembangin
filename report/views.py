28295809890218a80566cdd8f4b3e28f0f0b8e5f
arinaaunaka <arinaaunaka@gmail.com>
2022-12-09 06:45:35 +0700

    flutter adjustment

--- report/views.py
+++ report/views.py
@@ -11,28 +11,46 @@ def show_reports(request):
     form = ReportForm()
     return render(request, "report-progress.html", {'form':form})
 
-@login_required(login_url='/login/')
+#@login_required(login_url='/login/')
 def show_json(request):
-    data = Report.objects.filter(user=request.user)
-    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    list_of_reports = []
+    if request.user.is_authenticated:
+        reports = Report.objects.filter(user=request.user)
 
-@login_required(login_url='/login/')
+        for report in reports:
+            list_of_reports.append({
+                'pk':report.pk,
+                'date':report.date,
+                'name':report.name,
+                'age':report.age,
+                'height':report.height,
+                'weight':report.weight,
+                'eat':report.eat,
+                'drink':report.drink,
+                'progress':report.progress,
+            })
+        return JsonResponse(list_of_reports,safe=False)
+    else:
+        return HttpResponseBadRequest()
+
+#@login_required(login_url='/login/')
 @csrf_exempt
 def add_report(request):
     if request.method == 'POST':
         form = ReportForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
-            age = form.cleaned_data['age']
-            height = form.cleaned_data['height']
-            weight = form.cleaned_data['weight']
-            eat = form.cleaned_data['eat']
-            drink = form.cleaned_data['drink']
-            progress = form.cleaned_data['progress']
-            report = Report.objects.create(name=name, age=age, height=height, weight=weight, eat=eat, drink=drink, progress=progress, date=datetime.date.today(), user=request.user)
-            reports = {
-                'pk':report.pk,
-                'fields':{
+            if request.user.is_authenticated:
+                name = form.cleaned_data['name']
+                age = form.cleaned_data['age']
+                height = form.cleaned_data['height']
+                weight = form.cleaned_data['weight']
+                eat = form.cleaned_data['eat']
+                drink = form.cleaned_data['drink']
+                progress = form.cleaned_data['progress']
+                report = Report.objects.create(name=name, age=age, height=height, weight=weight, eat=eat, drink=drink, progress=progress, date=datetime.date.today(), user=request.user)
+                reports = {
+                    'status':True,
+                    'pk':report.pk,
                     'date':report.date,
                     'name':report.name,
                     'age':report.age,
@@ -42,9 +60,12 @@ def add_report(request):
                     'drink':report.drink,
                     'progress':report.progress,
                 }
-            }
-            return JsonResponse(reports)
-        return HttpResponseBadRequest()
+                return JsonResponse(reports)
+            else:
+                return JsonResponse({'status':False, 'message':'Anda harus login terlebih dahulu'})
+        else:
+            return JsonResponse({'status':False, 'message':"Input tidak valid!"})
+    return HttpResponseBadRequest()
 
 @login_required(login_url='/login/')
 def delete_report(request, id):
@@ -53,15 +74,13 @@ def delete_report(request, id):
         report.delete()
         reports = {
             'pk':report.pk,
-            'fields':{
-                'date':report.date,
-                'name':report.name,
-                'age':report.age,
-                'height':report.height,
-                'weight':report.weight,
-                'eat':report.eat,
-                'drink':report.drink,
-                'progress':report.progress,
-            }
+            'date':report.date,
+            'name':report.name,
+            'age':report.age,
+            'height':report.height,
+            'weight':report.weight,
+            'eat':report.eat,
+            'drink':report.drink,
+            'progress':report.progress,
         }
         return JsonResponse(reports)
