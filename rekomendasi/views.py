01a2327b9d5350bbc49649ae365f5563600c9412
Maria Aurellia <88271313+mariaadannies@users.noreply.github.com>
2022-12-12 17:51:26 +0700

    Revert "updated for flutter"

--- rekomendasi/views.py
+++ rekomendasi/views.py
@@ -15,7 +15,6 @@ import json
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from user_profile.views import show_profile
-from django.http import JsonResponse
 
 # Create your views here.
 # @login_required(login_url='/login')
@@ -27,44 +26,22 @@ def show_rekomendasi(request):
     }
     return render(request, "rekomendasi.html", context)
 
-# @login_required(login_url='/login')
+@login_required(login_url='/login')
 @csrf_exempt
 def add_rekomendasi(request):
     if request.method == "POST":
-        print(request.POST['nama_barang'])
-
-        data = request.POST
-
-        # data = json.loads(request.POST['data'])
-        # print(data)
-        # print(data['nama_barang'])
-        
+        data = json.loads(request.POST['data'])
 
         new_rekomendasi = Rekomendasi(nama_barang=data["nama_barang"], harga_barang=data["harga_barang"], deskripsi=data["deskripsi"], url=data["url"], gambar=data["gambar"])
         new_rekomendasi.save()
 
-        return JsonResponse({
-        "status": True,
-        "message": "Berhasil menambahkan rekomendasi"
-        # Insert any extra data if you want to pass data to Flutter
-        }, status=200)
+        return HttpResponse(serializers.serialize("json", [new_rekomendasi]), content_type="application/json")
 
-    return JsonResponse({
-        "status": False,
-        "message": "Cek kembali input anda"
-        # Insert any extra data if you want to pass data to Flutter
-        }, status=401)
+    return HttpResponse()
 
-# @login_required(login_url='/login')
-@csrf_exempt
+@login_required(login_url='/login')
 def show_json(request):
-    # rekomendasi = Rekomendasi.objects.filter()
-    rekomendasi = Rekomendasi.objects.all()
+    rekomendasi = Rekomendasi.objects.filter()
     data = serializers.serialize('json', rekomendasi)
 
-    # return JsonResponse({
-    #     "status": True,
-    #     "message": "Data berhasil diambil",
-        
-    # }, status=200)
     return HttpResponse(data, content_type='application/json')
